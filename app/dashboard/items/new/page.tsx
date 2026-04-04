d8dbb96d6927ddc5749d3413e940c9561dfb5909
Takano <edutakajr@gmail.com>
2026-04-04 14:36:21 -0300

    wip

--- app/dashboard/items/new/page.tsx
+++ app/dashboard/items/new/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import ItemForm from "@/components/items/ItemForm";
+import { Suspense } from "react";
 
 export default function NewItemPage() {
     return (
-        <ItemForm />
+        <Suspense fallback={<div>Carregando formulário...</div>}>
+            <ItemForm />
+        </Suspense>
     );
 }
